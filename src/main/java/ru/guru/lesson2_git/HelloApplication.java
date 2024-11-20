13c1b81e74aa5c629a3682ccca36e8273997c69f
kkaidd <90973712+kkaidd@users.noreply.github.com>
2024-11-20 22:51:13 +0300

    Update HelloApplication.java

--- src/main/java/ru/guru/lesson2_git/HelloApplication.java
+++ src/main/java/ru/guru/lesson2_git/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello QA-2!");
-        stage.setTitle("Hello QA-Edit_something_on_git!");
+        stage.setTitle("Hello QA-Edit_something_on_git!-Critical error");
         stage.setScene(scene);
         stage.show();
     }
